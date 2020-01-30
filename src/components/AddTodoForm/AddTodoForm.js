bcc93f3df0082834266458ad1204aa84c5e366e2
Nikita <ntrafilkin88@gmail.com>
2020-01-30 17:58:32 +0300

    adds adding new todo functionality, AddTodoForm transforms in class component

--- src/components/AddTodoForm/AddTodoForm.js
+++ src/components/AddTodoForm/AddTodoForm.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./AddTodoForm.css";
 
-const AddTodoForm = ({ addTodo }) => {
-  return (
-    <div className="item-add-form">
-      <button
-        onClick={() => addTodo("Do stuff")}
-        className="btn btn-outline-secondary"
-      >
-        Add Todo
-      </button>
-    </div>
-  );
-};
-
-export default AddTodoForm;
+export default class AddTodoForm extends Component {
+  state = {
+    label: ""
+  };
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.label);
+    this.setState({
+      label: ""
+    });
+  };
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} className="item-add-form d-flex">
+        <input
+          placeholder="what needs to be done"
+          onChange={this.onLabelChange}
+          type="text"
+          className="form-control"
+          value={this.state.label}
+        />
+        <button className="btn btn-outline-secondary text-nowrap">
+          Add Todo
+        </button>
+      </form>
+    );
+  }
+}
