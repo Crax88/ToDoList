8758e2678e8fa61ccd2571b1d0a454da4b04e22c
Nikita <ntrafilkin88@gmail.com>
2020-01-30 14:41:58 +0300

    adds state to TodoListItem with class

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./TodoListItem.css";
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-    fontWeight: important ? "bold" : "normal"
+export default class TodoListItem extends Component {
+  state = {
+    done: false
   };
-  return (
-    <span className="todo-list-item" style={style}>
-      <span className="todo-list-item-label">{label}</span>
-      <button className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-      <button className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+  render() {
+    const { label, important = false } = this.props;
+    const { done } = this.state;
+    let classNames = "todo-list-item";
+    if (done) classNames += " done";
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal"
+    };
+    return (
+      <span className={classNames} style={style}>
+        <span onClick={this.onLabelClick} className="todo-list-item-label">
+          {label}
+        </span>
+        <button className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+        <button className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
+}
